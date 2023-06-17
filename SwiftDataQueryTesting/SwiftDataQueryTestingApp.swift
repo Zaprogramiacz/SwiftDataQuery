7eb07c7a492ce7123c860da656d7b0f1476df764
Maciej Gomółka <maciej.gomolka@protonmail.com>
2023-06-17 17:07:29 +0200

    Initial commit

---
+++ SwiftDataQueryTesting/SwiftDataQueryTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataQueryTestingApp.swift
+//  SwiftDataQueryTesting
+//
+//  Created by Maciej Gomółka on 17.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataQueryTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
